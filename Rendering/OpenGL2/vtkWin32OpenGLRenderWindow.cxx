c3d8b80dcb3b8a676d1b4e7f87554f2aebb8d872
Fedor Moiseev <fedor.moiseev@flintgrp.com>
2021-10-15 12:56:37 +0200

    Fix offscreen rendering in OpenGL2

--- Rendering/OpenGL2/vtkWin32OpenGLRenderWindow.cxx
+++ Rendering/OpenGL2/vtkWin32OpenGLRenderWindow.cxx
@@ -1106,6 +1106,11 @@ int *vtkWin32OpenGLRenderWindow::GetSize(void)
 // Get the size of the whole screen.
 int *vtkWin32OpenGLRenderWindow::GetScreenSize(void)
 {
+  if (this->OffScreenRendering)
+    {
+    return this->Size;
+    }
+
   HDC hDC = ::GetDC(NULL);
   if (hDC)
     {
@@ -1418,6 +1423,10 @@ void vtkWin32OpenGLRenderWindow::CreateOffScreenWindow(int width,
 #endif
     this->CreateOffScreenDC(width,height,dc);
     DeleteDC(dc);
+    }
+    else
+    {
+      this->Mapped = 0;
     }
   this->CreatingOffScreenWindow = status;
 }
