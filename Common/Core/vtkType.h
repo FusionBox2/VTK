0aa19c482c7d11656580d41f1163348ce64cfde4
Fedor Moiseev <fedormsv@gmail.com>
2021-10-15 07:58:03 +0200

    Add vtkMTimeType typedef for future migration

--- Common/Core/vtkType.h
+++ Common/Core/vtkType.h
@@ -208,6 +208,8 @@ typedef signed long   vtkTypeInt64;
 # error "No native data type can represent a 64-bit integer."
 #endif
 
+typedef unsigned long vtkMTimeType;
+
 /* Select a 32-bit floating point type.  */
 #if VTK_SIZEOF_FLOAT == 4
 typedef float vtkTypeFloat32;
